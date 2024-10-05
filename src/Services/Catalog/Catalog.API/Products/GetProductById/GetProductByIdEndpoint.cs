37b46f55f8077e29f95e3c4aba4da2b27b5d9247
navid asadipur <navid.asadipur@gmail.com>
2024-10-05 22:04:13 +0330

    Added ProductGetByIdFeature. end of 6.15

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
@@ -0,0 +1,8 @@
+﻿namespace Catalog.API.Products.GetProductById;
+
+//public record GetProductByIdRequest();
+public record GetProductByIdResponse(Product Product);
+
+public class GetProductByIdEndpoint
+{
+}
