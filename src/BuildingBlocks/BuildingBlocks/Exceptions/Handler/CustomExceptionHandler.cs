05143a83673e411412343aec02f6e4669a032f6f
n.asadipour <navid.asadipur@gmail.com>
2024-10-09 18:23:03 +0330

    Added common exception in BuildingBlocks class library and modified ProductNotFoundException to inherite from it. Added UseExceptionHandler Middleware in program.cs. Added validation rules in delete and update product producs. end of 7.9

---
+++ src/BuildingBlocks/BuildingBlocks/Exceptions/Handler/CustomExceptionHandler.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+
+namespace BuildingBlocks.Exceptions.Handler;
+internal class CustomExceptionHandler : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(HttpContext context, Exception exception, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
