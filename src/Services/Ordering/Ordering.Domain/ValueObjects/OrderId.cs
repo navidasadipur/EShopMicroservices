b36c60c873b9b908d3ee77801973ca979f1284e9
n.asadipour <navid.asadipur@gmail.com>
2024-12-25 16:59:59 +0330

    End of section 14. Enrich Order and Product entity, value objects and add order events in domain project.

--- src/Services/Ordering/Ordering.Domain/ValueObjects/OrderId.cs
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/OrderId.cs
@@ -2,4 +2,16 @@
 public class OrderId
 {
     public Guid Value { get; }
+    private OrderId(Guid value) => Value = value;
+     
+    public static OrderId Of(Guid value)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+        if (value == Guid.Empty)
+        {
+            throw new DomainException("OrderId cannot be empty.");
+        }
+
+        return new OrderId(value);
+    }
 }
