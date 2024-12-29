5f00157afdb4a3834f57b8f75dbf2e71a44235cd
navid asadipur <navid.asadipur@gmail.com>
2024-12-29 20:06:27 +0330

    Add Ef Configurations for entities and add initial migration. end of 15.15

--- src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -9,9 +9,9 @@ public static class DependencyInjection
     {
         var connectionString = configuration.GetConnectionString("Database");
 
-        //// Add services to the container.
-        //services.AddDbContext<ApplicationDbContext>(options =>
-        //    options.UseSqlServer(connectionString));
+        // Add services to the container.
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(connectionString));
 
         //services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
 
